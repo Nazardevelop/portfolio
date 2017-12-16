2c7c109b2c4a73e0f7de466e2f6654760abec929
Nazar <tolmachovnazar@gmail.com>
2017-12-16 17:42:19 +0200

    some changes with slider, maybe wrong, i think to try slider from that source http://www.ravinderpayal.com/Simple-and-Light-Weight-Image-or-Content-Slider-for-Angular2/

---
+++ src/app/image-slider/mock-images.ts
@@ -0,0 +1,8 @@
+import { Image } from './Image';
+export const IMAGES = [
+    {url:"../../assets/images/html.jpg"},
+    {url:"../../assets/images/CSS.png"},
+    {url:"../../assets/images/JavaScript.png"},
+    {url:"../../assets/images/angularjs.jpg"},
+    {url:"../../assets/images/TypeScript.png"}
+];
